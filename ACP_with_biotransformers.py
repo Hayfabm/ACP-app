231c27396a3bc7176d50b1e84e2b4721c7ef8c08
Hayfabm <haifa.b.messaoud@gmail.com>
2021-10-28 17:26:18 +0000

    bio-transformers

--- ACP_with_biotransformers.py
+++ ACP_with_biotransformers.py
@@ -1,17 +1,17 @@
-"""ACP training script for custom model with one hot encoding"""
+"""ACP training script with bio-transformers"""
 import datetime
 import os
 import random
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import(
+from tensorflow.keras.layers import (
     InputLayer,
     Dense,
     Dropout,
     Flatten,
     Convolution1D,
-    MaxPooling1D
+    MaxPooling1D,
 )
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.callbacks import (
@@ -29,9 +29,9 @@ from biotransformers import BioTransformers
 def build_model(embedding_size, pool_length):
     """ANN_ACP model """
     custom_model = Sequential(name="ANN_ACP")
-    custom_model.add(InputLayer(input_shape=((embedding_size, 1)))) 
+    custom_model.add(InputLayer(input_shape=((embedding_size, 1))))
     # CNN_input  (None, 1024, 1)
-    # CNN_output (None, 1024, 32)  
+    # CNN_output (None, 1024, 32)
     custom_model.add(Dropout(0.1))
     custom_model.add(
         Convolution1D(
@@ -41,7 +41,7 @@ def build_model(embedding_size, pool_length):
             padding="same",
             activation="relu",
             kernel_initializer="random_uniform",
-            name="convolution_1d_layer1"
+            name="convolution_1d_layer1",
         )
     )
     custom_model.add(MaxPooling1D(pool_size=pool_length))
@@ -53,13 +53,9 @@ def build_model(embedding_size, pool_length):
     return custom_model
 
 
-
 if __name__ == "__main__":
     # init neptune logger
-    run = neptune.init(
-        project='sophiedalentour/ACP-app',
-        tags=['Dropout_0.1'],
-    )
+    run = neptune.init(project="sophiedalentour/ACP-app", tags=["Dropout_0.1"],)
 
     # set the seed
     SEED = 42
@@ -69,7 +65,7 @@ if __name__ == "__main__":
     tf.random.set_seed(SEED)
 
     # embedding and convolution parameters
-    EMBEDDING_SIZE= 1024
+    EMBEDDING_SIZE = 1024
     BIOTF_MODEL = "protbert"
     BIOTF_POOLMODE = "mean"
     BIOTF_BS = 2
@@ -109,21 +105,21 @@ if __name__ == "__main__":
         sequences_train, pool_mode=(BIOTF_POOLMODE,), batch_size=BIOTF_BS
     )[
         BIOTF_POOLMODE
-    ]  #(1378, 1024)
-  
+    ]  # (1378, 1024)
+
     sequences_test_embeddings = bio_trans.compute_embeddings(
         sequences_test, pool_mode=(BIOTF_POOLMODE,), batch_size=BIOTF_BS
     )[
         BIOTF_POOLMODE
-    ]  #(344, 1024)
+    ]  # (344, 1024)
 
-    # sequences reshape 
+    # sequences reshape
     sequences_train_embeddings = sequences_train_embeddings.reshape(
         sequences_train_embeddings.shape[0], 1024, 1
-    )  #(1378, 1024,1)
+    )  # (1378, 1024,1)
     sequences_test_embeddings = sequences_test_embeddings.reshape(
         sequences_test_embeddings.shape[0], 1024, 1
-    )  #(344, 1024,1)
+    )  # (344, 1024,1)
 
     # encode labels
     labels_train_encoded = to_categorical(
@@ -146,8 +142,8 @@ if __name__ == "__main__":
 
     # define callbacks
     my_callbacks = [
-        #ReduceLROnPlateau(monitor="val_loss", factor=0.1, patience=3, verbose=1),
-        #EarlyStopping(monitor="val_loss", min_delta=0, patience=5, verbose=1),
+        # ReduceLROnPlateau(monitor="val_loss", factor=0.1, patience=3, verbose=1),
+        # EarlyStopping(monitor="val_loss", min_delta=0, patience=5, verbose=1),
         ModelCheckpoint(
             monitor="val_accuracy",
             mode="max",
