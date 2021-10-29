9a99af0e4e51e83ef379028111684c57f32f64aa
Hayfabm <haifa.b.messaoud@gmail.com>
2021-10-29 09:44:33 +0000

    feature_add

--- Feature_generation.py
+++ Feature_generation.py
@@ -27,22 +27,22 @@ def AAC(seq, **kw):
 
 # Dipeptide composition
 def DPC(seq, **kw):
-    AA = 'ARNDCQEGHILKMFPSTWYV'
-    encodings = []
+	AA = kw['order'] if kw['order'] != None else 'ACDEFGHIKLMNPQRSTVWY'
+	encodings = []
 	#diPeptides = [aa1 + aa2 for aa1 in AA for aa2 in AA]
 	#header = ['#'] + diPeptides
 	#encodings.append(header)
 
-    AADict = {}
-    for i in range(len(AA)):
-        AADict[AA[i]] = i
+	AADict = {}
+	for i in range(len(AA)):
+		AADict[AA[i]] = i
+
 	for i in range(len(seq)):
-        sequence = seq[i]
+		sequence = seq[i]
 		code = []
 		tmpCode = [0] * 400
 		for j in range(len(sequence) - 2 + 1):
-			tmpCode[AADict[sequence[j]] * 20 + AADict[sequence[j+1]]
-            ] = tmpCode[AADict[sequence[j]] * 20 + AADict[sequence[j + 1]]] + 1
+			tmpCode[AADict[sequence[j]] * 20 + AADict[sequence[j+1]]] = tmpCode[AADict[sequence[j]] * 20 + AADict[sequence[j+1]]] +1
 		if sum(tmpCode) != 0:
 			tmpCode = [i/sum(tmpCode) for i in tmpCode]
 		code = code + tmpCode
